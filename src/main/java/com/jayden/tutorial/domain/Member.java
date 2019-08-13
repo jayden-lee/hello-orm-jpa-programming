12228d41caa62ecf125ccafa928a3901d33ec11d
jayden-lee <gwanggi.lee@gmail.com>
2019-08-13 23:17:15 +0900

    Add schema and data

---
+++ src/main/java/com/jayden/tutorial/domain/Member.java
@@ -0,0 +1,36 @@
+package com.jayden.tutorial.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    protected Member() {
+
+    }
+
+    @Builder
+    public Member(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
