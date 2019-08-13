bee4b3cb7c79802b1fb71a56856a9f6383709e6e
jayden-lee <gwanggi.lee@gmail.com>
2019-08-14 00:04:33 +0900

    Rename package

--- src/main/java/com/jayden/tutorial/domain/member/infra/MemberRepository.java
+++ src/main/java/com/jayden/tutorial/domain/member/infra/MemberRepository.java
@@ -1,5 +1,6 @@
-package com.jayden.tutorial.domain.member;
+package com.jayden.tutorial.domain.member.infra;
 
+import com.jayden.tutorial.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
